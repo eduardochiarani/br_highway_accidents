f95c1ff9d366739ec31e9417f6269e5a7eefd7d9
eduardochiarani <eduardo.chairani@gmail.com>
2024-11-18 10:41:08 -0300

    Correção de concatenação

--- utils/data_cleaning.py
+++ utils/data_cleaning.py
@@ -7,14 +7,11 @@ df3 = pd.read_csv("../data/2023.csv", sep=';', header=0, encoding='latin1')
 df4 = pd.read_csv("../data/2024.csv", sep=',', header=0)
 
 # Concatenar dataframes
-df_concat = pd.concat([df1, df2], axis=0, ignore_index=True)
-print(len(df_concat))
+df_concat = pd.concat([df1, df2, df3, df4], axis=0, ignore_index=True)
 
 # Retirar linhas NaN e duplicadas
 df_concat = df_concat.dropna()
-print(len(df_concat))
 df_concat = df_concat.drop_duplicates()
-print(len(df_concat))
 
 # Transformar data_inversa -> datetime -> dt.date
 df_concat['data_inversa'] = pd.to_datetime(df_concat['data_inversa'])
