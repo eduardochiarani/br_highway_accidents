94b6ba8521ca49d1c018d557e1d6c06fa8dd081f
eduardochiarani <eduardo.chairani@gmail.com>
2024-11-21 18:51:26 -0300

    restruturação do código

--- src/utils/data_cleaning.py
+++ src/utils/data_cleaning.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 # Leitura dataframes
-df1 = pd.read_csv("../data/2021.csv", sep=';', header=0, encoding='latin1')
-df2 = pd.read_csv("../data/2022.csv", sep=',', header=0)
-df3 = pd.read_csv("../data/2023.csv", sep=';', header=0, encoding='latin1')
-df4 = pd.read_csv("../data/2024.csv", sep=',', header=0)
+df1 = pd.read_csv("../../data/raw/2021.csv", sep=';', header=0, encoding='latin1')
+df2 = pd.read_csv("../../data/raw/2022.csv", sep=',', header=0)
+df3 = pd.read_csv("../../data/raw/2023.csv", sep=';', header=0, encoding='latin1')
+df4 = pd.read_csv("../../data/raw/2024.csv", sep=',', header=0)
 
 # Concatenar dataframes
 df_concat = pd.concat([df1, df2, df3, df4], axis=0, ignore_index=True)
@@ -19,3 +19,6 @@ df_concat['data_inversa'] = df_concat['data_inversa'].dt.date
 # Transformar horario -> datetime -> dt.time
 df_concat['horario'] = pd.to_datetime(df_concat['horario'], format='%H:%M:%S')
 df_concat['horario'] = df_concat['horario'].dt.time
+
+# Transformar br int64 -> object
+df_concat['br'] = df_concat['br'].astype('object')
