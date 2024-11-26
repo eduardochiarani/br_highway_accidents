b702929cd31a96e7cf64bd62c5ae3d83e0453516
Johnson <johnson.peixotoo@gmail.com>
2024-11-26 17:48:21 -0300

    feat: inicio do projeto

---
+++ src/dashboard.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Inicio",
+    page_icon="🏡",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items={"Get Help": None, "Report a bug": None, "About": None}
+)
+
+st.title('Bem vindos ao projeto!')
